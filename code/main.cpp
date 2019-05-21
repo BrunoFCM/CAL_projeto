de32df5e693796f7cf7080e5773619e86f4f6634
Pastilhas <joao.campos1221@gmail.com>
2019-05-21 11:51:00 +0100

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "bus.hpp"
 #include "tourist.hpp"
 
@@ -8,12 +9,37 @@
 
 bool loadTurists() {
     ifstream input; input.open(INPUT_FILE);
-    string line;
+    istringstream line; string info;
     /* T,id,name,<list of pois>                     TURIST  */
     /* B,id,cap                                     BUS     */
-    while(getline(input, line)){
-        if(line.at(0) == 'T'){
 
+    vector<Tourist*> tourists;
+    vector<Bus*> buses;
+    Tourist* tourist;
+    Bus* bus;
+
+    while (getline(input, info)) {
+        line.str(info);
+        getline(line, info, ',');
+        if (info == "T") {
+            int id;
+            string name;
+            getline(line, info, ',');
+            id = stoi(info);
+            getline(line, info, ',');
+            name = info;
+
+            tourist = new Tourist(id, name);
+
+        } else if (info == "B") {
+            int id;
+            unsigned long long cap;
+            getline(line, info, ',');
+            id = stoi(info);
+            getline(line, info, ',');
+            cap = stoi(info);
+
+            bus = new Bus(id, cap);
         }
     }
 }
