221cfb6209de69eedf800234e7d72b922271f600
Pastilhas <joao.campos1221@gmail.com>
2019-05-21 10:53:10 +0100

    push before fork bombed

--- code/main.cpp
+++ code/main.cpp
@@ -1,3 +1,18 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include "bus.hpp"
+#include "tourist.hpp"
+
+#define TURISTA_FILE "turista.txt"
+
+bool loadTurists() {
+    ifstream input; input.open(TURISTA_FILE);
+    string line;
+    /*T id name <list of pois>*/
+    while(getline(input, line)){
+        
+    }
+}
 
 int main() { return 0; }
